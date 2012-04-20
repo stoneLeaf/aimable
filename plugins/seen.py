8debd9aef0ad6b451f64c5e8b72f1f78a0e7a511
stoneLeaf <owi.stoneleaf@gmail.com>
2012-04-20 15:57:25 +0200

    Updated the seen plugin
    The .seen command now only fetches whereabouts of a nick in the chan it is issued. This enforce privacy.

--- plugins/seen.py
+++ plugins/seen.py
@@ -12,17 +12,18 @@ from util import hook
 
 
 def db_init(db):
-    db.execute("CREATE TABLE IF NOT EXISTS last_seen"
-               "(indexnick PRIMARY KEY, nick, chan, time, line REAL)")
+    db.execute("CREATE TABLE IF NOT EXISTS seen(lowernick, chan, nick, "
+               "time REAL, message, PRIMARY KEY(lowernick, chan))")
     db.commit()
 
-def db_query(db, indexnick):
-    return db.execute("SELECT nick, chan, time, line FROM last_seen "
-                      "WHERE indexnick = ?", (indexnick,)).fetchone()
+def db_fetch(db, lowernick, chan):
+    return db.execute("SELECT nick, time, message FROM seen WHERE "
+                      "lowernick = ? AND chan = ?", (lowernick, chan)
+                      ).fetchone()
 
-def db_record(db, indexnick, nick, chan, time, line):
-    db.execute("REPLACE INTO last_seen(indexnick, nick, chan, time, line) "
-               "values(?, ?, ?, ?, ?)", (indexnick, nick, chan, time, line))
+def db_insert(db, lowernick, chan, nick, time, message):
+    db.execute("REPLACE INTO seen(lowernick, chan, nick, time, message) "
+               "values(?, ?, ?, ?, ?)", (lowernick, chan, nick, time, message))
     db.commit()
 
 
@@ -31,11 +32,12 @@ time_units = (("year", 60 * 60 * 24 * 365), ("month", 60 * 60 * 24 * 30),
               ("second", 1))
 
 def time_since(then, now):
-    offset = int(now) - int(then)
+    """Determines and formats the time interval between then and now."""
+    offset = int(now) - int(then) if int(now) > int(then) else 0
     b = []
     for i, (name, duration) in enumerate(time_units):
         unit, offset = divmod(offset, duration)
-        if unit:
+        if unit or (offset == 0 and duration == 1):
             b.append("%d %s%s" % (unit, name, ("s" if unit > 1 else "")))
         if len(b) == 2:
             break
@@ -45,32 +47,32 @@ def time_since(then, now):
 @hook.singlethread
 @hook.event("PRIVMSG")
 def watch_nicks(inp, input=None, db=None):
-    # Don't record private messages
+    """Records nick, chan, time and text of every PRIVMSG."""
+    # Only record messages sent to chans
     if input.chan[:1] in ("#", "&", "+", "!"):
         db_init(db)
-        db_record(db, input.nick.lower(), input.nick,
-                  input.chan, time.time(), input.msg)
+        db_insert(db, input.nick.lower(), input.chan,
+                  input.nick, time.time(), input.msg)
 
 @hook.command
 def seen(inp, input=None, db=None, conn=None):
     """.seen <nick> -- Indicates when and where <nick> was last seen."""
-
     if inp.find(" ") != -1:
         return "You must provide a valid nick."
 
-    indexnick = inp.lower()
-    if conn.nick.lower() == indexnick:
+    lowernick = inp.lower()
+    if conn.nick.lower() == lowernick:
         return "I'm right here."
-    if input.nick.lower() == indexnick:
+    if input.nick.lower() == lowernick:
         return "You're right here."
 
     db_init(db)
-    query = db_query(db, indexnick)
+    query = db_fetch(db, lowernick, input.chan)
     if query is not None:
         who = "%s was" % query[0]
-        where = query[1]
-        when = "%s ago" % time_since(query[2], time.time())
-        what = query[3].replace("\x01ACTION", "me *").strip()
+        where = input.chan
+        when = "%s ago" % time_since(query[1], time.time())
+        what = query[2].replace("\x01ACTION", "me *").strip()
         return "%s last seen %s in %s saying: %s" % (who, when, where, what)
     else:
-        return "I've never seen %s chat." % inp
+        return "I've never seen %s chat in %s." % (inp, input.chan)
